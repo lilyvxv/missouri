26bdf95b3c65994631fa00ea8aa3d08e7d90e9ed
lilyvxv <>
2023-04-20 20:26:17 +0100

    code formatting

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import requests
-from faker import Faker
 from requests_toolbelt.multipart.encoder import MultipartEncoder
-import fuck_missouri
+from faker import Faker
+from lxml import html
+import requests
 import random
 import threading
 import time
 import traceback
-from lxml import html
+import fuck_missouri
 
 faker = Faker()
+
+SUBMIT_ENDPOINT = 'https://ago.mo.gov/file-a-complaint/transgender-center-concerns'
+THREAD_COUNT = 1
 
 def phone_number():
     return ''.join(str(random.randint(0, 9)) for _ in range(10))
@@ -18,19 +21,19 @@ def submit_form():
         try:
             # Solve the CAPTCHA and get the data to submit
             captcha_resp = fuck_missouri.generate_captcha_token()
-            print(f"Captcha solved | {captcha_resp['captcha-a']}")
+            print(f'Captcha solved | {captcha_resp["captcha-a"]}')
 
             # Construct the request
             form_data = {
-                "TextFieldController_4": faker.first_name(),
-                "TextFieldController_5": faker.last_name(),
-                "TextFieldController_1": faker.address().replace('\n', ' '),
-                "TextFieldController_2": faker.city(),
-                "DropdownListFieldController": random.choice(["ME", "MD", "MA", "MI", "MN", "MS"]),
-                "TextFieldController_6": faker.zipcode(),
-                "TextFieldController_0": faker.free_email(),
-                "TextFieldController_3": phone_number(),
-                "ParagraphTextFieldController": faker.sentence(),
+                'TextFieldController_4': faker.first_name(),
+                'TextFieldController_5': faker.last_name(),
+                'TextFieldController_1': faker.address().replace('\n', ' '),
+                'TextFieldController_2': faker.city(),
+                'DropdownListFieldController': random.choice(['ME', 'MD', 'MA', 'MI', 'MN', 'MS']),
+                'TextFieldController_6': faker.zipcode(),
+                'TextFieldController_0': faker.free_email(),
+                'TextFieldController_3': phone_number(),
+                'ParagraphTextFieldController': faker.sentence(),
             }
 
             form_data.update(captcha_resp)
@@ -38,9 +41,9 @@ def submit_form():
             parsed_form_data = multipart_data.to_string().decode()
 
             headers = {
-                "Content-Type": multipart_data.content_type,
-                "User-Agent": faker.user_agent(),
-                "X-Forwarded-For": faker.ipv4()
+                'Content-Type': multipart_data.content_type,
+                'User-Agent': faker.user_agent(),
+                'X-Forwarded-For': faker.ipv4()
             }
 
             # Send the request
@@ -51,27 +54,38 @@ def submit_form():
             )
 
             # Parse success from the response HTML
-            if "Success! Thanks for filling out our form!" in response.text:
-                print("Response successfully submitted")
+            if 'Success! Thanks for filling out our form!' in response.text:
+                print('Response successfully submitted')
                 continue
             
             # Parse possible error from the response HTML
             data = html.fromstring(response.text)
             reason = data.xpath('/html/body/main/div[2]/div[1]/div/div/div/text()')
+
             if reason != []:
-                print(f"Failed to submit response, reason: {reason[0]}")
+                print(f'Failed to submit response, reason: {reason[0]}')
             else:
-                print(f"Failed to submit response.")
-                with open('index.html', 'w') as fp:
-                    fp.write(response.text)
+                print(f'Failed to submit response')
         except:
             print(traceback.format_exc())
 
-for i in range(200):
-    threading.Thread(target=submit_form, daemon=True).start()
+def main():
+    # Create the threads
+    threads = []
+    for _ in range(THREAD_COUNT):
+        thread = threading.Thread(target=submit_form, daemon=True)
+        threads.append(thread)
 
-while True:
-    try:
-        time.sleep(0.1)
-    except KeyboardInterrupt:
-        exit(0)
+    # Start the threads
+    for thread in threads:
+        thread.start()
+
+    # Keep the main thread alive
+    while True:
+        try:
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            exit(0)
+
+if __name__ == "__main__":
+    main()
